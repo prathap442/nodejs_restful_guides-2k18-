1e7ca2c3102b7c845dbb11e2b5c18a31962543fa
prathap442 <pmohansaikrishna@gmail.com>
2021-06-09 10:39:35 +0530

    add: promises learning of the states

---
+++ file1.js
@@ -0,0 +1,30 @@
+//Promise that is being there in the pending state should eventually
+//fall into any of the states like ["resolve","reject"]
+//once the function falls into the resolve then it has successfully hit the api and use the keyword "resolve"
+//once the function falls inot the reject then the api call that we are making has failed and use the keyword "reject".
+
+
+const p = new Promise((resolve, reject)=>{
+    //put some async work that means that takes time to execute
+    setTimeout(()=>{
+      console.log("function is being called")
+      let value= Math.round(Math.random()*100)
+      console.log(value)
+      if(value % 2 == 0){
+        console.log('even number')
+        resolve("1")
+      }
+      else{
+        console.log('odd number')
+        reject(new Error("message of the odd number",value))  
+      }
+    },2000)
+})
+
+p
+  .then((result)=>{
+    console.log(result)
+  })
+  .catch((err)=>{
+    console.log(err.message)
+  })
