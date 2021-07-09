69a41aa8dc1874127a253c58774f7b3aabdb1ab8
prathap442 <pmohansaikrishna@gmail.com>
2021-07-09 23:18:53 +0530

    add: the connection to the mongodb database

--- file1.js
+++ file1.js
@@ -3,29 +3,41 @@ const mongoPort = 27017
 const mongodbUrl = `mongodb://${process.env.db_service_name}:${mongoPort}/${process.env.db_name}`
 console.log("the mongo db url is .....")
 console.log(mongodbUrl)
-
 const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 app.get('/',(request, response)=>{
    response.send("hello world") 
 })
-const db = mongoose.connection
-mongoose.connect(mongodbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 
+const db = mongoose.connection
 let connectWithRetry = function () {
   return mongoose.connect(mongodbUrl, {
     useUnifiedTopology: true,
     useNewUrlParser: true
-  });
+  })
+  .catch((err)=>{
+    console.log("the error that occured is", err)
+    connectWithRetry()
+  })
 };
 
-connectWithRetry()
-//now we connect to the data base once the db is connected then immediate thing is to connect tot the express server
-//runningo n the port 2525
+
+
+mongoose.connect(mongodbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(()=>{
+    console.log("the database is connected")
+  })
+  .catch((err)=>{
+    console.log()
+    connectWithRetry()
+    //now we connect to the data base once the db is connected then immediate thing is to connect tot the express server
+    //runningo n the port 2525
+  })
+
 db.on('connected',function(){  
   console.log("now connected to the database")  
-  express.listen(PORT,()=>{
+  app.listen(PORT,()=>{
      console.log(`Listening on the express port ${PORT}`) 
   })
   const Cat = mongoose.model('Cat', { name: String });
